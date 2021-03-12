664857511e97615fa39324d49a68e10dc50561ec
divesh-netizen <61093504+divesh-netizen@users.noreply.github.com>
2021-03-12 08:18:32 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
